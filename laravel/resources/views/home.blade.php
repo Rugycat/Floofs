41a0820a5ad7640c1dfdd7fed721a6c0044f347e
Rugycat <rugi.petr2003@gmail.com>
2025-12-15 10:22:38 +0200

    with env

--- laravel/resources/views/home.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>Welcome to Floofs Dashboard</h1>
-<p>Use the menu to browse Users, Pets, and Health Records.</p>
-@endsection
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,6 +6,3 @@ Route::get('/login', function () {
     return response()->json(['message' => 'Please use /auth/login (POST)'], 400);
 })->name('login.web');
 
-Route::get('/', function () {
-    return view('home');
-});
